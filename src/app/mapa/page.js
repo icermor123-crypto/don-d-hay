6863f25a065a5496ef079b96b30739b24eda8d60
icermor123-crypto <icermor123@gmail.com>
2026-05-02 15:56:02 -0400

    mapa limpio sin iconos de terceros

--- src/app/mapa/page.js
+++ src/app/mapa/page.js
@@ -57,7 +57,7 @@ export default function MapaPage() {
         attributionControl: true,
       })
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
       }).addTo(map)
